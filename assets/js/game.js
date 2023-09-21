7e15bab6251071451e991b002a4f3a641bb64a62
Engrloverboy <samsonanu@yahoo.com>
2023-09-21 15:51:45 +0100

    sound

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+var numberOfButtons = document.querySelectorAll(".btn");
+
+for (var i = 0; i < numberOfButtons.length; i++) {
+  numberOfButtons[i].addEventListener("click", handleClick);
+
+  function handleClick() {
+    var audio = new Audio("assets/sound/green.mp3");
+    audio.play();
+  }
+}
