3fce7985e18b5aef9786395ad1a4e901acaa8d02
Engrloverboy <samsonanu@yahoo.com>
2023-09-25 15:14:20 +0100

    add sound to colors

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,64 @@
+// const buttonColours = ["red", "blue", "green", "yellow"];
+
+// var GreenButton = document.querySelector(".green");
+// var RedButton = document.querySelector(".red");
+// var BlueButton = document.querySelector(".blue");
+// var YellowButton = document.querySelector(".yellow");
+
+// function GreenBTN() {
+//   var audio = new Audio(`assets/sound/green.mp3`);
+//   audio.play();
+// }
+// GreenButton.addEventListener("click", GreenBTN);
+
+// function RedBTN() {
+//   var audio = new Audio(`assets/sound/red.mp3`);
+//   audio.play();
+// }
+// RedButton.addEventListener("click", RedBTN);
+
+// function BlueBTN() {
+//   var audio = new Audio(`assets/sound/blue.mp3`);
+//   audio.play();
+// }
+// BlueButton.addEventListener("click", BlueBTN);
+
+// function YellowBTN() {
+//   var audio = new Audio(`assets/sound/yellow.mp3`);
+//   audio.play();
+// }
+// YellowButton.addEventListener("click", YellowBTN);
+
+
+
+
+
+
 const buttonColours = ["red", "blue", "green", "yellow"];
 
-var numberOfButtons = document.querySelectorAll(".btn");
-
-for (var i = 0; i < numberOfButtons.length; i++) {
-  numberOfButtons[i].addEventListener("click", handleClick);
-
-  function handleClick() {
-    var audio = new Audio("assets/sound/green.mp3");
-    audio.play();
-  }
+// Get references to the buttons using a loop
+const buttons = {};
+for (const color of buttonColours) {
+  buttons[color] = document.querySelector("." + color);
 }
+
+// Function to handle button clicks
+function handleButtonClick(color) {
+  const audio = new Audio(`assets/sound/${color}.mp3`);
+  audio.play();
+}
+
+// Add click event listeners to the buttons using the array
+for (const color of buttonColours) {
+  buttons[color].addEventListener("click", () => handleButtonClick(color));
+}
+
+
+// for (var i = 0; i < numberOfButtons.length; i++) {
+//   numberOfButtons[i].addEventListener("click", handleClick);
+
+//   function handleClick() {
+//     var audio = new Audio(`assets/sound/${buttonColours}.mp3`);
+//     audio.play();
+//   }
+// }
